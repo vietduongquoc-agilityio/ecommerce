cc0cb390afc5d7f9790ea9f7144997a5d25958c0
viet.duong <viet.duong@asnet.com.vn>
2025-02-19 14:39:10 +0700

    Deploy vercel

--- ecommerce/src/app/not-found.tsx
+++ ecommerce/src/app/not-found.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
 
 const NotFound = () => (
-  <div>
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding:"200px"
+    }}
+  >
     <h2>Not Found</h2>
     <p>Could not find requested resource</p>
     <Link href="/" className="underline">
--- ecommerce/src/services/apiClient.ts
+++ ecommerce/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://holy-nurture-ad1450cf59.strapiapp.com/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
