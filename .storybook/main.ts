50ce79b8fa77df9f0e6ed1cce2005a222a0baef9
viet.duong <viet.duong@asnet.com.vn>
2025-02-24 14:01:30 +0700

    Updated storybook and unit test for components

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,4 @@
-// import type { StorybookConfig } from "@storybook/nextjs";
-
-// const config: StorybookConfig = {
-//   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-//   addons: [
-//     "@storybook/addon-onboarding",
-//     "@storybook/addon-essentials",
-//     "@chromatic-com/storybook",
-//     "@storybook/addon-interactions",
-//   ],
-//   framework: {
-//     name: "@storybook/nextjs",
-//     options: {},
-//   },
-//   staticDirs: ["..\\public"],
-// };
-// export default config;
-
 import type { StorybookConfig } from '@storybook/nextjs';
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -32,12 +13,6 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['../public'],
-  webpackFinal: async (config) => {
-    if (config.resolve) {
-      config.resolve.plugins = [new TsconfigPathsPlugin()];
-    }
-    return config;
-  },
 };
 
 export default config;
